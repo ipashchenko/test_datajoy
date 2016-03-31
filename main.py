ca116dec30d21a664633c6add6e4fa470be9fccb
Ilya <in4pashchenko@gmail.com>
2016-03-31 11:57:28 +0400

    Initial ShareLaTeX Import

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2 * np.pi, 100)
+
+plt.plot(x, np.sin(x))
+plt.show()
+
+import scipy as sp
+import emcee
